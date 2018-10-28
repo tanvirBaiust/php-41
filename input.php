fe5b71082597ea708967aec69445c6464bd4b887
tanvirBaiust <tanvircse469@gmail.com>
2018-10-28 21:03:55 +0600

    create layout input.php file

---
+++ input.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+
+<html>
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+    <body>
+        <table>
+            <tr>
+                <td>Enter your Element height</td>
+                <td><input type="number" id="height"></td>
+            </tr>
+            <tr>
+                <td>Enter your Element width</td>
+                <td><input type="number" id="width"></td>
+            </tr>
+            <tr>
+                <td>Enter your background colour</td>
+                <td><input type="color" id="colour"></td>
+            </tr>
+            <tr>
+                <td>Enter your radius</td>
+                <td><input type="number" id="radius"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" id="btn" value="submit"></td>
+            </tr>
+        </table>
+        <hr/>
+        <span>Your element is:</span>
+        <div id="new_element"></div>
+        <script src="new.js"></script>
+    </body>
+</head>
+</html>
